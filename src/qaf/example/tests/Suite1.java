2618db7193480a0a433d174bc0db405cb525f1ab
SyuuRifu <syuurifu@icloud.com>
2025-05-10 13:06:55 +0800

    更改

---
+++ src/qaf/example/tests/Suite1.java
@@ -0,0 +1,20 @@
+package qaf.example.tests;
+
+import org.testng.annotations.Test;
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+import static com.qmetry.qaf.automation.step.CommonStep.*;
+
+public class Suite1 extends WebDriverTestCase {
+
+    @Test(description="Sample Test Scenario", groups={"SMOKE"})
+    public void testGoogleSearch() {
+        get("/");
+        sendKeys("Git reporsitory QAF", "txt.searchbox");
+        submit("txt.searchbox");
+        verifyLinkWithPartialTextPresent("qaf");
+        System.out.println("test@@@@@@@@@@@@");
+    }
+
+
+
+}
